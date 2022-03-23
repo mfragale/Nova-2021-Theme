a5f1712ea267a68e07fdf89a7015c878201928c6
Mauricio Fragale <mfragale@gmail.com>
2022-03-23 10:26:46 -0300

    blog

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php get_header(); ?>
 
-<div class="container px-4 py-5" id="custom-cards">
-    <h2 class="pb-2 border-bottom">Blog</h2>
 
-    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+<?php get_template_part('template-parts/posts/post-content'); ?>
 
 
-        <?php get_template_part('template-parts/posts/post-content'); ?>
-
-
-        <?php if ($wp_query->max_num_pages > 1) { ?>
-            <div class="btn btn-primary misha_loadmore">More posts</div>
-        <?php } ?>
-
-    </div>
-</div>
-
-
-
+<?php if ($wp_query->max_num_pages > 1) { ?>
+    <div class="btn btn-primary misha_loadmore">More posts</div>
+<?php } ?>
 
 
 <?php get_footer(); ?>
