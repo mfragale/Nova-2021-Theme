33e21359b118978c05b4838ca956aee7d401a4e8
Mauricio Fragale <mfragale@gmail.com>
2022-03-25 21:51:06 -0300

    gulp 4

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,19 +1,20 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    cleanCss     = require('gulp-clean-css'),
-    rename       = require('gulp-rename'),
-    postcss      = require('gulp-postcss'),
+var gulp = require('gulp'),
+    sass = require('gulp-sass')(require('sass')),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    postcss = require('gulp-postcss'),
     autoprefixer = require('autoprefixer');
 
-const { src, dest }  = require("gulp");
+const { src, dest } = require("gulp");
 const minify = require("gulp-minify");
 
 function buildCss() {
     return gulp.src(['../scss/*.scss'])
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(postcss([ autoprefixer({ overrideBrowserslist: [
+        .pipe(postcss([autoprefixer({
+            overrideBrowserslist: [
                 'Chrome >= 35',
                 'Firefox >= 38',
                 'Edge >= 12',
@@ -22,24 +23,25 @@ function buildCss() {
                 'Safari >= 8',
                 'Android 2.3',
                 'Android >= 4',
-                'Opera >= 12']})]))
+                'Opera >= 12']
+        })]))
         // .pipe(sourcemaps.write())
         // .pipe(gulp.dest('scss/dist/'))
         .pipe(cleanCss())
-        .pipe(rename({suffix: '-min'}))
+        .pipe(rename({ suffix: '-min' }))
         .pipe(gulp.dest('../scss/dist/'))
 }
 
 
 function minifyjs() {
-    return src('../js/functions.js', { allowEmpty: true }) 
-        .pipe(minify({noSource: true}))
+    return src('../js/functions.js', { allowEmpty: true })
+        .pipe(minify({ noSource: true }))
         .pipe(dest('../js/dist'))
 }
 
 function watcher() {
-  gulp.watch(['../scss/*.scss', '../js/*.js'],
-  gulp.series(buildCss, minifyjs));
+    gulp.watch(['../scss/*.scss', '../js/*.js'],
+        gulp.series(buildCss, minifyjs));
 }
 
 exports.watch = gulp.series(buildCss, watcher, minifyjs);
