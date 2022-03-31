cb67a1236d312f6265013839388f0c56b74d56f2
Mauricio Fragale <mfragale@gmail.com>
2022-03-31 00:25:10 -0300

    Blog

--- single.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<article id="post-<?php the_ID(); ?>" class="<?php post_class(); ?>">
-    <div class="content">
-        <?php the_content(); ?>
-    </div>
-</article>
-
-<?php get_footer(); ?>
