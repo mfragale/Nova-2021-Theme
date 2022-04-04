4ac562c4d8cbd3e10434a87f7ef2b5d6b245b480
Mauricio Fragale <mfragale@gmail.com>
2022-04-03 23:19:53 -0300

    deleted post content

--- functions.php
+++ functions.php
@@ -273,46 +273,6 @@ add_shortcode('locais', 'locais');
 
 
 
-
-
-// function misha_my_load_more_scripts() //https://rudrastyh.com/wordpress/load-more-posts-ajax.html
-// {
-
-// 	global $wp_query;
-
-// 	// now the most interesting part
-// 	// we have to pass parameters to myloadmore.js script but we can get the parameters values only in PHP
-// 	// you can define variables directly in your HTML but I decided that the most proper way is wp_localize_script()
-// 	wp_localize_script('nova2021-functions', 'misha_loadmore_params', array(
-// 		'ajaxurl' => site_url() . '/wp-admin/admin-ajax.php', // WordPress AJAX
-// 		'posts' => json_encode($wp_query->query_vars), // everything about your loop is here
-// 		'current_page' => get_query_var('paged') ? get_query_var('paged') : 1,
-// 		'max_page' => $wp_query->max_num_pages
-// 	));
-// }
-// add_action('wp_enqueue_scripts', 'misha_my_load_more_scripts');
-
-// function misha_loadmore_ajax_handler()
-// {
-
-// 	// prepare our arguments for the query
-// 	$args = json_decode(stripslashes($_POST['query']), true);
-// 	$args['paged'] = $_POST['page'] + 1; // we need next page to be loaded
-// 	$args['post_status'] = 'publish';
-
-// 	// it is always better to use WP_Query but not here
-// 	query_posts($args);
-
-// 	get_template_part('template-parts/posts/post-content');
-
-// 	die; // here we exit the script and even no wp_reset_query() required!
-// }
-// add_action('wp_ajax_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_{action}
-// add_action('wp_ajax_nopriv_loadmore', 'misha_loadmore_ajax_handler'); // wp_ajax_nopriv_{action}
-
-
-
-
 /**
  * Create Logo Setting and Upload Control - https://getflywheel.com/layout/how-to-add-options-to-the-wordpress-customizer/
  */
