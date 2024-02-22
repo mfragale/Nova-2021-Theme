1437072fed5a24503dd1cd01833515dec1c67b1e
Mauricio Fragale <mfragale@gmail.com>
2024-02-22 16:30:57 -0300

    Remove non theme related files

--- js/functions.js
+++ js/functions.js
@@ -19,7 +19,6 @@ jQuery(function () {
 
 
 	$('.misha_loadmore').click(function () {
-
 		var button = $(this),
 			data = {
 				'action': 'loadmore',
@@ -51,28 +50,6 @@ jQuery(function () {
 			}
 		});
 	});
-
-
-
-
-
-	// Autoplays any video with the css class .lwp-video-autoplay
-	if ($('.lwp-video-autoplay .et_pb_video_box').length !== 0) {
-		$('.lwp-video-autoplay .et_pb_video_box').find('video').prop('muted', true);
-		$(".lwp-video-autoplay .et_pb_video_box").find('video').attr('loop', 'loop');
-		$(".lwp-video-autoplay .et_pb_video_box").find('video').attr('playsInline', '');
-
-		$(".lwp-video-autoplay .et_pb_video_box").each(function () {
-			$(this).find('video').get(0).play();
-		});
-		$('.lwp-video-autoplay .et_pb_video_box').find('video').removeAttr('controls');
-	}
-
-
-
-
-
-
 
 
 
